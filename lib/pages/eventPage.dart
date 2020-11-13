88359a6f15d12f2f1d3155f8515127a8f2099fdc
Krause-Torres <A.Krause-Torres@myeosgroup.net>
2020-11-13 16:12:05 +0100

    added pages

--- lib/pages/eventPage.dart
+++ lib/pages/eventPage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class NewEventPage extends StatelessWidget {
+class EventPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
